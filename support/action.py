04a0f8670a3c21c14ae407bc5d269f4965a1b5c1
nihlaeth <info@nihlaeth.nl>
2015-03-30 23:23:35 +0200

    Added action container/messenger class

---
+++ support/action.py
@@ -0,0 +1,17 @@
+class Action():
+    def __init__(self, cls, arguments):
+        """ Container class.
+        
+        Action acts as a kind of messenger between classes. This results in 
+        decreased complexity as by simply passing a class and a bunch of 
+        arguments around, the sender of this action doesn't have to worry 
+        about initialization, and the return of that initialization can be
+        left to the recipient of said action. This is basically delayed 
+        execution.
+
+        Arguments:
+        cls -- class
+        arguments -- dict of arguments needed to initialize the class
+        """
+        self.cls = cls
+        self.arguments = arguments
