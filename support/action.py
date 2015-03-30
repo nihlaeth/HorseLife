c7998dc2a02a855c9f16c912c72f5c8c7193a6f5
nihlaeth <info@nihlaeth.nl>
2015-03-30 23:27:04 +0200

    Formatting corrections (pep8)

--- support/action.py
+++ support/action.py
@@ -1,12 +1,12 @@
 class Action():
     def __init__(self, cls, arguments):
         """ Container class.
-        
-        Action acts as a kind of messenger between classes. This results in 
-        decreased complexity as by simply passing a class and a bunch of 
-        arguments around, the sender of this action doesn't have to worry 
+
+        Action acts as a kind of messenger between classes. This results in
+        decreased complexity as by simply passing a class and a bunch of
+        arguments around, the sender of this action doesn't have to worry
         about initialization, and the return of that initialization can be
-        left to the recipient of said action. This is basically delayed 
+        left to the recipient of said action. This is basically delayed
         execution.
 
         Arguments:
