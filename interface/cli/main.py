eafb614141a0496dd7d9a7c7927d54be37c4e7d0
nihlaeth <info@nihlaeth.nl>
2015-03-30 17:45:40 +0200

    Main screen

---
+++ interface/cli/main.py
@@ -0,0 +1,12 @@
+from display import Display
+
+
+class Main(Display):
+    def __init__(self):
+        Display.__init__(self)
+        self._title = "HorseLife"
+        self._description = (
+            "Welcome to HorseLife!"
+            ""
+            "Below is a list of buildings on your property. To enter one,"
+            "type their associated number and hit enter!")
