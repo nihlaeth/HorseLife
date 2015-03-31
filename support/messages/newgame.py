13c3587c1c2b24ab9939bc555656195fd4af4f68
nihlaeth <info@nihlaeth.nl>
2015-03-31 11:26:43 +0200

    Added name property

--- support/messages/newgame.py
+++ support/messages/newgame.py
@@ -3,5 +3,8 @@ from message import Message
 
 class NewGame(Message):
     """ Message to indicate creation of a new game."""
+    def __init__(self, name=False):
+        self.name = name
+    
     def __str__(self):
         return "New Game"
