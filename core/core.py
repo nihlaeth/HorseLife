1f368adf7846bd2326befb12728c5b813813105e
nihlaeth <info@nihlaeth.nl>
2015-03-31 11:36:08 +0200

    Basics for the core layer

---
+++ core/core.py
@@ -0,0 +1,14 @@
+class Core():
+    def __init__(self):
+        self._display = None
+
+    def run(self):
+        # get needed data, pass it to display class and activate it
+        # wait for response and handle it:
+        # call new class, hide display class, or 
+        # return appropriate action to the class that called this one
+        pass
+
+    def __str__(self):
+        return "Core"
+
