24867b84e3a5971591d0d7019cf2eca58050c0a1
nihlaeth <info@nihlaeth.nl>
2015-04-01 20:55:08 +0200

    Correct message handling

--- core/buildingcore.py
+++ core/buildingcore.py
@@ -3,6 +3,7 @@ from support.action import Action
 from support.messages.quit import Quit
 #from stablecore import StableCore
 
+
 class BuildingCore(Core):
     def __init__(self, building):
         # No display class here, since this core class only relays
@@ -10,17 +11,18 @@ class BuildingCore(Core):
         self._building = building
 
     def run(self):
-        if self._building.building_type == "Stable" :
-            next_ = StableCore(self._building)
-        else:
-            # unimplemented building type
-            next_ = None
+        while True:
+            if self._building.building_type == "Stable":
+                next_ = StableCore(self._building)
+            else:
+                # unimplemented building type
+                next_ = None
 
-        choice = next_.run()
-
-        # TODO figure out what to do with the input
-
-        return choice
+            choice = next_.run()
+            if isinstance(choice, Back) or isinstance(choice, Quit):
+                return choice
+            # Right now there isn't really any other option, but
+            # for consistency's sake, this runs in a loop anyway.
 
     def __str__(self):
         return self._building.name
--- core/maincore.py
+++ core/maincore.py
@@ -10,20 +10,21 @@ class MainCore(Core):
         self._display = MainDisplay()
 
     def run(self):
-        buildings = BuildingsBackend.all()
-        actions = []
-        for b in buildings:
-            actions.append(BuildingCore(b))
+        while True:
+            buildings = BuildingsBackend.all()
+            actions = []
+            for b in buildings:
+                actions.append(BuildingCore(b))
 
-        menu = []
-        menu.append(Quit())
+            menu = []
+            menu.append(Quit())
 
-        self._display.init(actions, menu)
-        choice = self._display.display()
-
-        # TODO: decide what to do with the input
-
-        return choice
+            self._display.init(actions, menu)
+            choice = self._display.display()
+            if isinstance(choice, Quit):
+                return choice
+            # If it's anything else, just display
+            # the main screen again.
 
     def __str__(self):
         return "Main"
