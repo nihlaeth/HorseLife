90397a83f470f55c030380cd89f758bfff919efd
nihlaeth <info@nihlaeth.nl>
2015-04-01 21:28:49 +0200

    Added stable display

---
+++ interface/cli/stabledisplay.py
@@ -0,0 +1,12 @@
+from display import Display
+
+
+class StableDisplay(Display):
+    def __init__(self):
+        Display.__init__(self)
+        self._title = Stable
+        self._description = (
+                "Welcome to the stable!\n\n"
+                "Here you can groom your horse,"
+                "take it for a ride, and check"
+                "how it's doing.")
