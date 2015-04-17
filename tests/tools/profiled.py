2af5da90994b4daa1ff2d6ed0829b29ae53cc8c8
nihlaeth <info@nihlaeth.nl>
2015-04-17 16:18:32 +0200

    Have profiled dump stats to file suited for RunSnakeRun viewing

--- tests/tools/profiled.py
+++ tests/tools/profiled.py
@@ -9,13 +9,11 @@ def profiled(echo=False):
     if echo:
         pr = cProfile.Profile()
         pr.enable()
+
         yield
         pr.disable()
         s = StringIO.StringIO()
         ps = pstats.Stats(pr, stream=s).sort_stats('cumulative')
-        ps.print_stats()
-        # uncomment this to see who's calling what
-        # ps.print_callers()
-        print s.getvalue()
+        ps.dump_stats("profile-output.log")
     else:
         yield
