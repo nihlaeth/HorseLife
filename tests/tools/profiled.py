c39fbc04ea42aff50e03dc641a9d8b83acd7d8de
nihlaeth <info@nihlaeth.nl>
2015-04-17 10:38:34 +0200

    Made profiled silent by default to speed up testing

--- tests/tools/profiled.py
+++ tests/tools/profiled.py
@@ -5,14 +5,17 @@ import contextlib
 
 
 @contextlib.contextmanager
-def profiled():
-    pr = cProfile.Profile()
-    pr.enable()
-    yield
-    pr.disable()
-    s = StringIO.StringIO()
-    ps = pstats.Stats(pr, stream=s).sort_stats('cumulative')
-    ps.print_stats()
-    # uncomment this to see who's calling what
-    # ps.print_callers()
-    print s.getvalue()
+def profiled(echo=False):
+    if echo:
+        pr = cProfile.Profile()
+        pr.enable()
+        yield
+        pr.disable()
+        s = StringIO.StringIO()
+        ps = pstats.Stats(pr, stream=s).sort_stats('cumulative')
+        ps.print_stats()
+        # uncomment this to see who's calling what
+        # ps.print_callers()
+        print s.getvalue()
+    else:
+        yield
