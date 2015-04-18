490c7ff684106fc11c00caabedf79c3f19fae63b
nihlaeth <info@nihlaeth.nl>
2015-04-18 16:34:39 +0200

    Also test file listing thingy

--- tests/core/testloadcore.py
+++ tests/core/testloadcore.py
@@ -10,9 +10,11 @@ from support.messages.command import Command
 
 
 class TestLoadCore():
+    @mock.patch("os.listdir")
+    @mock.patch.object(LoadDisplay, "init")
     @mock.patch.object(LoadDisplay, "get_string")
     @mock.patch.object(LoadDisplay, "display")
-    def test_run(self, m1, m2):
+    def test_run(self, m1, m2, m3, m4):
         """ Test LoadCore.run()"""
         core = LoadCore()
         # Test SavedGame
@@ -49,3 +51,17 @@ class TestLoadCore():
             m1.assert_called_with()
         else:
             assert False
+
+        # Test directory listing
+        m4.return_value = ["file1", "file2"]
+        m1.return_value = Quit()
+        core.run()
+        args = m3.call_args_list[-1:]
+        arg1 = args[0][0][0]
+        arg2 = args[0][0][1]
+        print "arg1=" + str(arg1)
+        print "arg2=" + str(arg2)
+        assert_equals(arg1[0].file_name, "file1")
+        assert_equals(arg1[1].file_name, "file2")
+        assert_equals(isinstance(arg2[0], NewGame), True)
+        assert_equals(isinstance(arg2[1], Quit), True)
