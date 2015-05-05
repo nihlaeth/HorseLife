654bee2822c825181181dbeca5839016c4c2df9f
nihlaeth <info@nihlaeth.nl>
2015-05-05 10:12:57 +0200

    Add min level to Action.

--- support/messages/action.py
+++ support/messages/action.py
@@ -9,13 +9,14 @@ class Action(Message):
     Container for actions, for example clean a stable, groom a horse.
     """
 
-    def __init__(self, action, description, arguments=None):
+    def __init__(self, action, description, arguments=None, level=0):
         """Set action, description and optional arguments."""
         self.action = action
         self.description = description
         if arguments is None:
             arguments = []
         self.arguments = arguments
+        self.min_level = level
 
     def __str__(self):
         """Return string representation of object."""
